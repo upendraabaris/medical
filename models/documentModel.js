df95a291b35772e46d552317c8759811354467f4
upendraabaris <upendra.abaris@gmail.com>
2024-04-05 13:48:28 +0530

    vitalinfo and document

--- models/documentModel.js
+++ models/documentModel.js
@@ -1,15 +1,42 @@
-const mongoose  = require("mongoose");
+const mongoose = require("mongoose");
 
 const userDocumentSchema = new mongoose.Schema({
-    insurance_policy: { type: String, required: true},
-    // adhaar_front_card: { type: String, require: [true, "Adhaar card Front doc is a must"] },
-    // adhaar_back_card: { type: String, require: [true, "Adhaar card Back doc is a must"] },
-    // pan_card: { type: String, require: [true, "Pan card doc is a must"] },
-    // gst: { type: String },
-    // bank_proof: { type: String, require: [true, "Bank Proof is a must"] },
+    category: { type: String/* , required: true */ },
+    complaints: { type: String/* , required: true */ },
+    clinicalDiagnosis: { type: String/* , required: true */ },
+    doctorName: { type: String/* , required: true */ },
+    hospitalClinicName: { type: String/* , required: true */ },
+    fileDate: { type: Date/* , required: true */ },
+    feeAmountPaid: { type: Number/* , required: true */ },
+    fileTitle: { type: String/* , required: true */ },
+    isLatest: { type: Boolean, default: false },
+    reportType: { type: String/* , required: true */ },
+    linkDoctorPrescription: { type: String },
+    pathologyLabName: { type: String/* , required: true */ },
+    categoryDocumentType: { type: String/* , required: true */ },
+    selectFamilyMember: { type: String },
+    vaccinationType: { type: String/* , required: true */ },
+    expiryDate: { type: Date/* , required: true */ },
+    insurerCompanyName: { type: String/* , required: true */ },
+    insuranceType: { type: String, /* enum: ['Health/Medical Insurance', 'Life Insurance'], */ },
+    selectProcedure: { type: String },
+    document: [{ type: String , required: true }],
+    // insurance_policy: [{ type: String, required: true }],
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", index: true }
 }, {
     timestamps: true
 });
 
 module.exports = mongoose.model("userDocument", userDocumentSchema)
+
+
+
+
+
+
+
+// adhaar_front_card: { type: String, require: [true, "Adhaar card Front doc is a must"] },
+// adhaar_back_card: { type: String, require: [true, "Adhaar card Back doc is a must"] },
+// pan_card: { type: String, require: [true, "Pan card doc is a must"] },
+// gst: { type: String },
+// bank_proof: { type: String, require: [true, "Bank Proof is a must"] },
--- models/patientVitalInfoModel.js
+++ models/patientVitalInfoModel.js
@@ -3,18 +3,18 @@ const mongoose = require('mongoose');
 const patientVitalInfoSchema = new mongoose.Schema({
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },
     height: {
-        feet: { type: Number, required: true },
-        inches: { type: Number, required: true }
+        feet: { type: Number/* , required: true */ },
+        inches: { type: Number/* , required: true */ }
     },
-    weight: { type: Number, required: true },
-    pulseRate: { type: Number, min: 60, max: 100, required: true },
-    respiratoryRate: { type: Number, min: 12, max: 16, required: true },
-    bodyTemperature: { type: Number, min: 90, max: 105, required: true },
+    weight: { type: Number/* , required: true */ },
+    pulseRate: { type: Number, min: 60, max: 100/* , required: true */ },
+    respiratoryRate: { type: Number, min: 12, max: 16/* , required: true */ },
+    bodyTemperature: { type: Number, min: 90, max: 105/* , required: true */ },
     bloodPressure: {
-        systolic: { type: Number, required: true },
-        diastolic: { type: Number, required: true }
+        systolic: { type: Number/* , required: true */ },
+        diastolic: { type: Number/* , required: true */ }
     },
-    oxygenSaturation: { type: Number, min: 70, max: 100, required: true },
+    oxygenSaturation: { type: Number, min: 70, max: 100/* , required: true */ },
     dateTime: { type: Date, default: Date.now }
 },
 {
